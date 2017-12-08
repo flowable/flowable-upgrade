ad96675718b285d7fae7d95481baa56454782072
Tijs Rademakers <tijs.rademakers@gmail.com>
2017-12-08 21:01:30 +0100

    Rename because of script class name invocation:

--- flowable-upgrade-6.2.1/src/main/java/org/activiti/upgrade/DataGenerator.java
+++ flowable-upgrade-6.2.1/src/main/java/org/activiti/upgrade/DataGenerator.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.flowable.upgrade;
+package org.activiti.upgrade;
 
 import java.util.HashMap;
 import java.util.Map;
