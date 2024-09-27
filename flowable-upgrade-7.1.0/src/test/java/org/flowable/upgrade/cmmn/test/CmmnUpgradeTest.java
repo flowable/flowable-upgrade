ae7e6c234a92c9c4497036f69680f4a8234d7e94
Filip Hrisafov <filip.hrisafov@gmail.com>
2024-09-27 22:42:35 +0200

    Upgrade expected schema version to latest 7.2.0.0

--- flowable-upgrade-7.1.0/src/test/java/org/flowable/upgrade/cmmn/test/CmmnUpgradeTest.java
+++ flowable-upgrade-7.1.0/src/test/java/org/flowable/upgrade/cmmn/test/CmmnUpgradeTest.java
@@ -40,7 +40,7 @@ public class CmmnUpgradeTest extends UpgradeTestCase {
     		
     	});
     	
-    	assertEquals("7.1.0.1", cmmnSchemaVersion);
+    	assertEquals("7.2.0.0", cmmnSchemaVersion);
         
     }
 }
