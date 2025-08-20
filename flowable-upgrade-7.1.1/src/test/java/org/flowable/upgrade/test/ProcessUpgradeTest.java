45152da065dc7e340b3f64c3a5ea22c61075fbb9
Filip Hrisafov <filip.hrisafov@gmail.com>
2025-08-20 17:43:02 +0200

    Add test for expected process schema version

---
+++ flowable-upgrade-7.1.1/src/test/java/org/flowable/upgrade/test/ProcessUpgradeTest.java
@@ -0,0 +1,45 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flowable.upgrade.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.flowable.common.engine.impl.interceptor.Command;
+import org.flowable.common.engine.impl.interceptor.CommandContext;
+import org.flowable.common.engine.impl.persistence.entity.PropertyEntity;
+import org.flowable.common.engine.impl.persistence.entity.PropertyEntityManager;
+import org.junit.Test;
+
+public class ProcessUpgradeTest extends UpgradeTestCase {
+
+    @Test
+    public void testVersion() {
+    	String processSchemaVersion = appEngineConfiguration.getCommandExecutor().execute(new Command<String>() {
+
+			@Override
+			public String execute(CommandContext commandContext) {
+				PropertyEntityManager propertyEntityManager = appEngineConfiguration.getPropertyEntityManager();
+				PropertyEntity propertyEntity = propertyEntityManager.findById("schema.version");
+				if (propertyEntity != null) {
+					return propertyEntity.getValue();
+				}
+				
+				return null;
+			}
+    		
+    	});
+    	
+    	assertEquals("7.2.0.2", processSchemaVersion);
+        
+    }
+}
